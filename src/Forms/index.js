dd383e319ae874f5506012f7fff3da369c1a1817
Atanas Mahony <atanas@codeberry.io>
2020-07-01 20:06:42 +0300

    removing forms

--- src/Forms/index.js
+++
@@ -1,5 +0,0 @@
-export { StyledInput, Input } from './Input';
-export { Select } from './Select';
-export { StyledTextarea, Textarea } from './Textarea';
-export { RadioGroup, RadioButton } from './Radio';
-export { CheckboxGroup, CheckboxButton } from './Checkbox';
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ export {default as Button} from './Button';
 export {default as Card} from './Card';
 export {default as Divider} from './Divider';
 export {default as Dropdown} from './Dropdown';
-//forms
+
 export {default as Group} from './Group';
 export {default as Heading} from './Heading';
 export {default as Icon} from './Icon';
@@ -17,7 +17,7 @@ export {default as Progress} from './Progress';
 export {SegmentList, Segment} from './Segment';
 
 export {default as Svg} from './Svg';
-export {default as Tabs, TabList, Tab, TabPanel} from './Tabs';
+export {Tabs, TabList, Tab, TabPanel} from './Tabs';
 export {default as Tag} from './Tag';
 export {default as Text} from './Text';
 export {default as Theme} from './Theme';
