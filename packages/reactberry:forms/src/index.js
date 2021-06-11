9ea960dfb9452808c5b26953e3fb492a8a6ec0cf
Atanas Mahony <atanas@codeberry.io>
2021-06-11 12:16:21 +0300

    fix export

--- packages/reactberry:forms/src/index.js
+++ packages/reactberry:forms/src/index.js
@@ -3,5 +3,6 @@ export {default as Field} from './Field';
 export {StyledInput, Input} from './Input';
 export {default as Textarea} from './Textarea';
 export {RadioGroup, RadioButton} from './Radio';
-export {CheckboxGroup, CheckboxButton} from './Checkbox';
+export {CheckboxGroup} from './Checkbox';
+export {default as CheckboxButton} from './Checkbox';
 export {default as Select} from './Select';
